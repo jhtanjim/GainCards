b43b8220999367bbb22cfa7c11c339562fed1df9
jhtanjim <jh.tanjim.4@gmail.com>
2025-05-05 23:49:26 -0700

    done

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,27 +1,11 @@
-import { createContext, useContext, useState, useEffect } from "react";
+import React from 'react'
 
-const ShopContext = createContext();
-
-export const useShop = () => useContext(ShopContext);
-
-export const ShopProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState(() => {
-    const storedCart = localStorage.getItem("cartItems");
-    return storedCart ? JSON.parse(storedCart) : [];
-  });
-
-  const [clientSecret, setClientSecret] = useState("");
-
-  // Update localStorage whenever cartItems change
-  useEffect(() => {
-    localStorage.setItem("cartItems", JSON.stringify(cartItems));
-  }, [cartItems]);
-
+const ShopContext = () => {
   return (
-    <ShopContext.Provider
-      value={{ cartItems, setCartItems, clientSecret, setClientSecret }}
-    >
-      {children}
-    </ShopContext.Provider>
-  );
-};
+    <div>
+      
+    </div>
+  )
+}
+
+export default ShopContext
