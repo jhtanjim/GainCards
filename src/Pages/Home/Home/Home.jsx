2651cac625fa4cb5e21a1ac0bf295b59c0669647
jhtanjim <jh.tanjim.4@gmail.com>
2025-06-05 23:27:10 -0700

    check loading homepage

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,26 +1,23 @@
-import React from 'react'
-import Pokaemon from '../../Shared/Pokaemon/Pokaemon'
-import Banner from '../Banner/Banner'
-import Banner1 from '../Banner/Banner1'
-import About from '../About/About'
-import PokemonCardsCollection from '../PokemonCardsCollection/PokemonCardsCollection'
-import Subscription from '../Subscription/Subscription'
-import DonateSection from '../DonateSection/DonateSection'
-import WhyChooseUs from '../WhyChooseUs/WhyChooseUs'
+import Banner from "../Banner/Banner";
+
+import About from "../About/About";
+import PokemonCardsCollection from "../PokemonCardsCollection/PokemonCardsCollection";
+import Subscription from "../Subscription/Subscription";
+import DonateSection from "../DonateSection/DonateSection";
+import WhyChooseUs from "../WhyChooseUs/WhyChooseUs";
 
 const Home = () => {
   return (
-<div className="min-h-screen">  
+    <div className="min-h-screen">
       <Banner className="bg-black" />
-      {/* <Banner1/> */}
-      <PokemonCardsCollection/>
-      <Subscription/>
-      <About/>
-      <DonateSection/>
-      <WhyChooseUs/>
-      {/* <Pokaemon/> */}
-    </div>
-  )
-}
 
-export default Home
+      <PokemonCardsCollection />
+      <Subscription />
+      <About />
+      <DonateSection />
+      <WhyChooseUs />
+    </div>
+  );
+};
+
+export default Home;
