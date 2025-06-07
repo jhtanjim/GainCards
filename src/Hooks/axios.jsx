6458855c59af140eaa4a774396c56fa12a1f4e94
jhtanjim <jh.tanjim.4@gmail.com>
2025-06-07 13:51:38 -0700

    add livelink

--- src/Hooks/axios.jsx
+++ src/Hooks/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://gaincards.com/",
+  baseURL: "https://gaincards.com/api",
   withCredentials: true,
   timeout: 10000, // Add timeout
 });
