09d140c24b47be772ba7a0165b04329788dc5697
jhtanjim <jh.tanjim.4@gmail.com>
2025-06-17 19:10:19 -0700

    signInwith pass

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -34,7 +34,7 @@ import DonateCardReceiver from "../Pages/Shared/Donate/DonateCardReciever/Donate
 import Pokemon from "../Pages/Shared/Pokemon/Pokemon";
 import PokemonCardDetails from "../Pages/Shared/Pokemon/PokemonCardDetails";
 import PokemonCardUpdate from "../Pages/Shared/PokemonCardCrud/PokemonCardUpdate";
-import SignInwithForgetPass from "../auth/signInwithForgetPass.jsx";
+import SignInwithForgetPass from "../auth/signInwithForgetPass";
 export const router = createBrowserRouter([
   {
     path: "/",
